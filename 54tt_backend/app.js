a70d36baeb181add4ca48d051e7644b1415fef34
sharme <yaomiwang@qq.com>
2018-06-16 21:30:01 +0800

    remove express.compress

--- 54tt_backend/app.js
+++ 54tt_backend/app.js
@@ -46,7 +46,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.compress());
+// app.use(express.compress());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
